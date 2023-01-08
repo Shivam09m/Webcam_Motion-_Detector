ec4ff532b79d177b7219346340db94349d0acd0d
Shivam Mehrotra <shivammehrotra09@yahoo.in>
2023-01-09 01:08:10 +0530

    initial commit

---
+++ emailing.py
@@ -0,0 +1,29 @@
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+
+PASSWORD = "pjnpvfraolhxrxen"
+EMAIL_SENDER = "mehrotrashivam09@gmail.com"
+EMAIL_RECEIVER = "mehrotrashivam09@gmail.com"
+
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New Customer Showed Up!"
+    email_message.set_content("Hey, We just saw a new customer!")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 578)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(EMAIL_SENDER, PASSWORD)
+    gmail.sendmail(EMAIL_SENDER, EMAIL_RECEIVER, email_message.as_string())
+    gmail.quit()
+
+
+if __name__ == "__main__":
+    send_email(image_path="images/24.png")
